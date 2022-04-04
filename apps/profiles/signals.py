de43abcd98d8caa235323d6a9245a975ad437f5e
Mahabubul Hasan <shourav6688@gmail.com>
2022-04-04 22:25:42 +0600

    feat: user authentication with jwt

--- apps/profiles/signals.py
+++ apps/profiles/signals.py
@@ -6,13 +6,14 @@ from apps.profiles.models import Profile
 
 logger = logging.getLogger(__name__)
 @receiver(post_save,sender=AUTH_USER_MODEL)
-
 def create_user_profile(sender,instance,created,**kwargs):
     if created:
         Profile.objects.create(user=instance)
+        logger.info(f"{instance}'s profile created successfully")
 
 @receiver(post_save,sender=AUTH_USER_MODEL)
 def save_user_profile(sender,instance,**kwargs):
     instance.profile.save()
-    logger.info(f"{instance}'s profile created successfully")
+    logger.info(f"{instance}'s profile updated successfully")
+
 
--- real_estate/urls.py
+++ real_estate/urls.py
@@ -4,6 +4,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path('superadmin/', admin.site.urls),
+    path("api/v1/auth/", include("djoser.urls")),
+    path("api/v1/auth/", include("djoser.urls.jwt")),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
@@ -11,3 +13,5 @@ urlpatterns = [
 admin.site.site_header = "Real Estate Admin"
 admin.site.site_title = "Real Estate Admin Portal"
 admin.site.index_title = "Welcome to the Real Estate Portal"
+
+
